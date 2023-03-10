9748e3a834c469ce6cbfd2a090ecc6795aaf8f14
bruno <brunoaugustine2@gmail.com>
2023-03-10 12:50:53 +0100

    fix responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import flag from '../assets/flag.png'
 import { SlArrowDown } from 'react-icons/sl'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { FaTimes } from "react-icons/fa"
+import icon from "../assets/icon.png"
 
 const Navbar = (props) => {
 
@@ -13,6 +14,18 @@ const Navbar = (props) => {
             {/* ---------------------- dropdown --------------- */}
             {views && <div className={`bg-white h-screen w-full`}>
                 <div className="w-fit ml-auto cursor-pointer text-red-600 text-xl p-3" onClick={() => setViews(!views)}> <FaTimes /> </div>
+
+                <div className='flex flex-col gap-4 px-3'>
+                    <Link to='/' className=''>
+                        <img src={icon} className='' alt='' />
+                    </Link>
+                    <div className=""> <Link to='/savings_and_investment' className={`capitalize text-black font-semibold`}>products</Link> </div>
+                    <div className=""> <Link to='/business' className={`capitalize text-black font-semibold`}>business</Link> </div>
+
+                    <div className=""> <Link to='' className={`capitalize text-black font-semibold`}>faq</Link> </div>
+                    <div className="flex items-center gap-2"> <img src={flag} className='w-7 h-7 rounded-full' alt="" /> <SlArrowDown className={`text-black text-xs`} /> </div>
+                    <div className={`bg-blue-600 text-white w-fit text-sm rounded-full py-2.5 px-4 font-semibold`}>Download the Credpal app</div>
+                </div>
             </div>}
             <div style={{ backgroundColor: `${props.bgcolor}` }}>
                 <div className='flex py-6 px-3 mx-auto container items-center justify-between'>
@@ -37,7 +50,7 @@ const Navbar = (props) => {
                                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                             </button>
                         </div>
-                        <div className='hidden md:flex md:gap-x-5'>
+                        <div className='hidden md:flex items-center md:gap-x-5'>
                             <div className=""> <Link to='' className={`capitalize text-${props.textcolor} font-semibold`}>faq</Link> </div>
                             <div className="flex items-center gap-2"> <img src={flag} className='w-7 h-7 rounded-full' alt="" /> <SlArrowDown className={`text-${props.textcolor} text-xs`} /> </div>
                             <div className={`${props.buttoncolor} text-sm rounded-full py-2.5 px-4 font-semibold`}>Download the Credpal app</div>
